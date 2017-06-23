089998f37ebe7f97c8a8895d1d4703487f2f1ff8
Gino Jongenelen <jongenelengino@gmail.com>
2017-06-23 02:26:22 +0200

    comment second radiusdb dir in config

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ if [[ -h /etc/freeradius/mods-enabled/sql ]]; then
 fi
 
 sed -i "s/allow_vulnerable_openssl.*/allow_vulnerable_openssl = yes/" /etc/freeradius/radiusd.conf
+sed -i "s/radius_db.*/#radius_db = \"radius\"/" /etc/freeradius/mods-available/sql
 sed -i '0,/md5/{s/md5/mschapv2/}' /etc/freeradius/mods-available/eap
 
 sed -i -e "s/driver =.*/driver = \"rlm_sql_mysql\"/" -e "s/dialect =.*/dialect = \"mysql\"/" /etc/freeradius/mods-available/sql
