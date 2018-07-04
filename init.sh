43a5e6d52fc741d750486f6b3d50433d4a20ed56
Gino <jongenelengino@gmail.com>
2018-07-04 10:25:21 +0200

    Update init.sh
    import certificate

--- init.sh
+++ init.sh
@@ -6,4 +6,10 @@ sed -i -e "/client localhost/i client 0.0.0.0/0{\n\tsecret = $radpass\n}" \
 sql_driver='mysql'
 sed -i "/driver =.*/ a\ \n\tserver = \"$mysql_server\"\n\tlogin = \"$mysql_login\"\n\tpassword = \"$mysql_passwd\"\n\tradius_db = \"$mysql_database\"" /etc/freeradius/mods-available/sql
 
+
+radius_certificate=$(redis-cli -h redis.web.svc.cluster.local -p 6379 get radius.pushto.space:latest)
+echo "$radius_certificate" | python3 -c "import sys, json; print(json.load(sys.stdin)['fullchain_pem'])" > /home/fullchain.pem
+echo "$radius_certificate" | python3 -c "import sys, json; print(json.load(sys.stdin)['privkey_pem'])" > /home/privkey.pem
+
+
 /usr/sbin/freeradius -f -X
